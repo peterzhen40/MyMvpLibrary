8c0bc0f718d0ca38d5db2ca984566af2dd09387e
peterzhen <yjzhen1991@163.com>
2019-05-24 10:00:27 +0800

    v1.1.9:iOS对话框样式优化

--- app/src/main/java/cn/peterzhen/demo/MainActivity.java
+++ app/src/main/java/cn/peterzhen/demo/MainActivity.java
@@ -1,13 +1,27 @@
 package cn.peterzhen.demo;
 
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
+import butterknife.BindView;
 import cn.cbsd.mvplibrary.mvp.XActivity;
+import cn.cbsd.mvplibrary.widget.IosDialog;
 import es.dmoral.toasty.MyToast;
 
 public class MainActivity extends XActivity {
+
+    @BindView(R.id.button1)
+    Button mButton1;
+    @BindView(R.id.button2)
+    Button mButton2;
+    @BindView(R.id.textView)
+    TextView mTextView;
+    @BindView(R.id.activity_main)
+    LinearLayout mActivityMain;
 
     @Override
     public int getLayoutId() {
@@ -16,17 +30,31 @@ public class MainActivity extends XActivity {
 
     @Override
     public void initData(Bundle savedInstanceState) {
-        MyToast.init(getApplication(),true,true);
+        MyToast.init(getApplication(), true, true);
 
-        Button button = findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
+        mButton1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //                Intent intent = new Intent(MainActivity.this, ListActivity.class);
                 //                startActivity(intent);
-                getvDelegate().show("获取数据错误");
-//                MyToast.show("获取数据错误");
+                new AlertDialog.Builder(context)
+                        .setTitle("提示")
+                        .setMessage("获取数据异常")
+                        .setPositiveButton("我知道了",null)
+                        .setNegativeButton("取消",null)
+                        .show();
+                //                MyToast.show("获取数据错误");
             }
+        });
+
+        mButton2.setOnClickListener(view -> {
+            new IosDialog(context).builder()
+                    .setTitle("提示")
+                    .setMessage("获取数据异常")
+                    .setPositiveButton("我知道了",null)
+                    .setNegativeButton("取消",null)
+                    .show();
+//            new AlertDialog.Builder()
         });
     }
 

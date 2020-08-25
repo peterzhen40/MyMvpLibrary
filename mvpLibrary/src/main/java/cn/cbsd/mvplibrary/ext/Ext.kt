af41d09548864b7c6c5abbe53f0846003ff3f41f
PeterZhen <yjzhen1991@gmail.com>
2020-08-25 15:43:11 +0800

    V3.0.9Release:降低Okhttp和Retrofit版本（OKHttp4.0需要API21+，不要随便升底层库版本），更新Ext

--- mvpLibrary/src/main/java/cn/cbsd/mvplibrary/ext/Ext.kt
+++ mvpLibrary/src/main/java/cn/cbsd/mvplibrary/ext/Ext.kt
@@ -1,10 +1,12 @@
 package cn.cbsd.mvplibrary.ext
 
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.disposables.Disposable
 import java.math.BigInteger
 import java.security.MessageDigest
 
 /**
- * 当前类注释:
+ * 当前类注释: 自定义扩展函数
  * @author zhenyanjun
  * @date   2019-12-12 09:00
  */
@@ -15,6 +17,44 @@ import java.security.MessageDigest
 fun String?.getNotNull():String{
     return if (isNullOrEmpty()) ""
     else this!!
+}
+
+fun Double?.getNotNull(): Double {
+    return this ?: 0.0
+}
+fun Int?.getNotNull(): Int {
+    return this ?: 0
+}
+
+fun Boolean?.getNotNull(): Boolean {
+    return this ?: false
+}
+
+fun <T> Collection<T>?.isNotNullAndNotEmpty(): Boolean = !isNullOrEmpty()
+
+fun <T> Collection<T>?.isNotNull(): Boolean = this != null
+
+fun <T> List<T>.toArrayList() :ArrayList<T>{
+    val arrayList = arrayListOf<T>()
+    arrayList.addAll(this)
+    return arrayList
+}
+
+/**
+ * rxjava Disposable处理
+ */
+fun Disposable.addTo(cd: CompositeDisposable) {
+    cd.add(this)
+}
+
+/**
+ * 交换元素
+ */
+fun <T> MutableList<T>.swap(index1: Int, index2: Int):MutableList<T> {
+    val tmp = this[index1]
+    this[index1] = this[index2]
+    this[index2] = tmp
+    return this
 }
 
 /**
